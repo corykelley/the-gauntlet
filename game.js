24320363595c9923fc7d1f2e417469997df95cb1
johncoryk <coryk0789@gmail.com>
2020-07-20 21:57:12 -0400

    Added more game logic, healing, attacking, and enemy basic ai

--- game.js
+++ game.js
@@ -1,8 +1,9 @@
 //Create game variables including classes for players and enemies.
-// -- Variables might include a globla random number generator (function), function to lower case all inputs, etc.
 let play = true;
 let attack = true;
 
+//Attack Modifier: Int between 5 and 30
+//Defense Modifier: Int between 2 and 5
 class Player {
   constructor(
     name,
@@ -32,6 +33,26 @@ class Player {
   attack() {
     const randomNum = Math.floor(Math.random() * 10 + 1);
     return this.attackAtr * randomNum + this.attMod;
+  }
+
+  heal() {
+    if (this.health === 100) {
+      alert(`You're already at full health!`);
+    } else {
+      const randomNum = Math.floor(Math.random() * 10 + 1);
+      const healingPoints = this.defMod * randomNum;
+      if (this.health + healingPoints > 100) {
+        this.health = 100;
+        alert(
+          `You've healed yourself by ${healingPoints} points. Your health is now at ${this.health}.`
+        );
+      } else {
+        this.health += healingPoints;
+        alert(
+          `You've healed yourself by ${healingPoints} points. Your health is now at ${this.health}.`
+        );
+      }
+    }
   }
 }
 
@@ -74,68 +95,117 @@ class Enemy {
     const randomNum = Math.floor(Math.random() * 10 + 1);
     return this.attackAtr * randomNum + this.attMod;
   }
+
+  heal() {
+    const randomNum = Math.floor(Math.random() * 10 + 1);
+    const healingPoints = this.defMod * randomNum;
+    if (this.health + healingPoints > 100) {
+      this.health = 100;
+      alert(
+        `The ${this.name} casts a healing spell, his health is now at ${this.health}.`
+      );
+    } else {
+      this.health += healingPoints;
+      alert(
+        `The ${this.name} casts a healing spell, his health is now at ${this.health}.`
+      );
+    }
+  }
 }
 
 const goblin = new Enemy('Goblin', 'Basic Club', 4, 3, 0, 3, 5, 2);
 
+const compareRolls = (playerRoll, enemyRoll, player, enemy) => {
+  if (playerRoll > enemyRoll) {
+    const goblinHealthLost =
+      Math.round(playerRoll / enemy.defenseAtr) + enemy.defMod;
+    enemy.health -= goblinHealthLost;
+    alert(`You struck the goblin, he lost ${goblinHealthLost} health!`);
+    console.log('Hero Health', player.health);
+    console.log('Goblin Health', enemy.health);
+  } else if (playerRoll < enemyRoll) {
+    const heroHealthLost =
+      Math.round(enemyRoll / player.defenseAtr) + player.defMod;
+    player.health -= heroHealthLost;
+    alert(`The goblin struck you, you lost ${heroHealthLost} health!`);
+    console.log('Hero Health', player.health);
+    console.log('Goblin Health', enemy.health);
+  } else {
+    alert(`Your weapons meet and you both fly backwards!`);
+  }
+};
+
 const battle = (player, enemy) => {
   let userInput = prompt(
-    `You've encountered a ${enemy.name}, what will you do? OPTIONS: Fight or Heal`
+    `You've encountered a ${enemy.name}, what will you do? OPTIONS: Fight or Heal?`
   );
-  userInput = userInput.toLocaleLowerCase();
+  userInput = userInput.toLowerCase();
   if (userInput === 'fight') {
-    const heroRoll = player.attack();
-    const goblinRoll = enemy.attack();
-    console.log('Hero roll', heroRoll);
-    console.log('Goblin Roll', goblinRoll);
-    if (heroRoll > goblinRoll) {
-      const goblinHealthLost =
-        Math.round(heroRoll / enemy.defenseAtr) + enemy.defMod;
-      enemy.health -= goblinHealthLost;
-      alert(`You struck the goblin, he lost ${goblinHealthLost}!`);
-      console.log('Hero Health', player.health);
-      console.log('Goblin Health', enemy.health);
-    } else if (heroRoll < goblinRoll) {
-      const heroHealthLost =
-        Math.round(goblinRoll / player.defenseAtr) + player.defMod;
-      player.health -= heroHealthLost;
-      alert(`The goblin struck you, you lost ${heroHealthLost}!`);
-      console.log('Hero Health', player.health);
-      console.log('Goblin Health', enemy.health);
+    let heroRoll = player.attack();
+    if (enemy.health < 50) {
+      let randomNum = Math.floor(Math.random() * 10 + 1);
+      if (randomNum % 2 === 0) {
+        enemy.heal();
+        randomNum = Math.floor(Math.random() * 10 + 1);
+        randomNum % 2 === 1 ? (goblinRoll = heroRoll) : (goblinRoll = 0);
+        compareRolls(heroRoll, goblinRoll, player, enemy);
+      } else {
+        let goblinRoll = enemy.attack();
+        compareRolls(heroRoll, goblinRoll, player, enemy);
+      }
     } else {
-      console.log(`You weapons meet and you both fly backwards!`);
+      let goblinRoll = enemy.attack();
+      console.log('Hero roll', heroRoll);
+      console.log('Goblin Roll', goblinRoll);
+      compareRolls(heroRoll, goblinRoll, player, enemy);
     }
   } else if (userInput === 'heal') {
-    player.health += 10;
-    alert(
-      `Your healed yourself by 10 points, your health is now at ${player.health}`
-    );
-    const heroRoll = player.defenseAtr + player.defMod;
-    const goblinRoll = enemy.attack();
-    console.log('Hero roll', heroRoll);
-    console.log('Goblin Roll', goblinRoll);
-    if (heroRoll < goblinRoll) {
-      const heroHealthLost =
-        Math.round(goblinRoll / player.defenseAtr) + player.defMod;
-      player.health -= heroHealthLost;
-      alert(`The goblin struck you, you lost ${heroHealthLost}!`);
-      console.log('Hero Health', player.health);
-      console.log('Goblin Health', enemy.health);
+    if (enemy.health < 20) {
+      randomNum = Math.floor(Math.random() * 10 + 1);
+      if (randomNum > 3) {
+        player.heal();
+        enemy.heal();
+        return;
+      } else {
+        player.heal();
+        const heroRoll = player.defenseAtr + player.defMod;
+        const goblinRoll = enemy.attack();
+        console.log('Hero roll', heroRoll);
+        console.log('Goblin Roll', goblinRoll);
+      }
+    } else {
+      player.heal();
+      const heroRoll = player.defenseAtr + player.defMod;
+      const goblinRoll = enemy.attack();
+      console.log('Hero roll', heroRoll);
+      console.log('Goblin Roll', goblinRoll);
+      if (heroRoll < goblinRoll) {
+        const heroHealthLost =
+          Math.round(goblinRoll / player.defenseAtr) + player.defMod;
+        player.health -= heroHealthLost;
+        alert(`The goblin struck you, you lost ${heroHealthLost} health!`);
+        console.log('Hero Health', player.health);
+        console.log('Goblin Health', enemy.health);
+      }
     }
   }
 };
 
-alert(`You have encountered a Goblin! Time to fight!`);
-while (play) {
-  if (goblin.health <= 0 || hero.health <= 0) {
-    hero.health > goblin.health
-      ? alert(`The day is yours!`)
-      : alert(`You've been defeated...`);
-    play = false;
-  } else {
-    battle(hero, goblin);
+const mainGame = () => {
+  alert(`You have encountered a Goblin! Time to fight!`);
+  while (play) {
+    if (goblin.health <= 0 || hero.health <= 0) {
+      hero.health > goblin.health
+        ? alert(`The day is yours!`)
+        : alert(`You've been defeated...`);
+      play = false;
+    } else {
+      battle(hero, goblin);
+    }
   }
-}
+};
+
+mainGame();
 
 //Create main battle sequence logic.
 // -- Create functions for parts of main battle.
